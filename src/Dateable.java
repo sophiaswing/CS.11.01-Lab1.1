eb98864b029a3f52451aaf6b7ae5353c47bc9c93
sophiaswing <sophiaks2026@student.cis.edu.hk>
2024-01-31 09:16:36 +0800

    done!

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+        int sophia=age/2+7;
+        System.out.println(age+" year olds should date someone who is at least "+sophia+" years old");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+        int sophia=100-age;
+        System.out.println(age+" year olds should read at least "+sophia+" pages before giving up on a book");
     }
 }
